e9ad00314387d2cd6f283b6946cf7775caa1dda8
Yogesh <123430440+Yogesh1501@users.noreply.github.com>
2024-08-19 10:51:32 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "writetoritika" # Replace with your actual S3 bucket name
+    bucket = "Yogesh2000" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-2"
   }
