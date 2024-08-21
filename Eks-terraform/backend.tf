d9fde2731cc1501e67716a4e941734e0265506ad
Nitinkumar Aswar <nitinkumaraswar94@gmail.com>
2024-08-21 12:15:53 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "yogesh2000" # Replace with your actual S3 bucket name
+    bucket = "nitin8766" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    region = "us-east-1"
   }
 }
